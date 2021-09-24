ffcb150c93f96ef37a928a565722bbf6d666dfa0
lock <nikitka.mukhin.04@gmail.com>
2021-09-24 16:43:46 +0300

    answer

---
+++ hw1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя:");
+
+            string name = Console.ReadLine();
+
+            Console.Write($"Привет, {name}, сегодня {DateTime.Now}");
+        }
+    }
+}
